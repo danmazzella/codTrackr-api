8a2cf05659afc27fdee735a952402cfd9b7e55d4
Dan Mazzella <danmazzella@gmail.com>
2020-06-02 15:53:20 -0400

    Create last fetch if not exist

--- mongo/helpers/options.helper.js
+++ mongo/helpers/options.helper.js
@@ -7,11 +7,21 @@ const Options = mongoose.model('Options');
 // Utils
 const { isNllOrUnd } = require('../../utils/validator');
 
+// Datatypes
+const { LAST_DATA_FETCH } = require('../../datatypes/options.datatypes');
+
 // Redis
 const { deleteFromRedis, getFromRedis, setInRedis } = require('../../redis/redis.helpers');
 const { LAST_FETCH } = require('../../redis/keys');
 
 const OptionsHelpers = {
+  createNewLastFetch: obj => new Promise((resolve, reject) => new Options(obj)
+    .save()
+    .then((res) => {
+      deleteFromRedis(LAST_FETCH);
+      return resolve(res);
+    })
+    .catch(err => reject(err))),
   findLastFetch: (obj, opts = {}, sort = {}) => new Promise(async (resolve) => {
     const options = await getFromRedis(LAST_FETCH);
 
@@ -21,7 +31,15 @@ const OptionsHelpers = {
 
     return Options
       .findOne(obj, opts, sort)
-      .then((data) => {
+      .then(async (_data) => {
+        let data = _data;
+        if (data === null) {
+          data = await OptionsHelpers.createNewLastFetch({
+            key: LAST_DATA_FETCH,
+            value: new Date(0).getTime(),
+          });
+        }
+
         setInRedis(LAST_FETCH, data);
         return resolve(data);
       })
@@ -32,7 +50,7 @@ const OptionsHelpers = {
       .updateOne(
         findObj,
         updateObj,
-        { upsert: true },
+        { upsert: true, new: true },
       )
       .then((data) => {
         deleteFromRedis(LAST_FETCH);
